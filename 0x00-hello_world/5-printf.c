a7d711a58afb0e07f0f4b938717f7c3a47ec02cc
gabbismart <brightgabi@gmail.com>
2022-03-11 14:45:14 -0800

    My C programming continue

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print the phrase with proper grammer,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properlu, non-zero otherwise
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
