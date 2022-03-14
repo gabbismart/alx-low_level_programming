6c9a0c885c6c56da503505dd905b27c94a6ee654
gabbismart <brightgabi@gmail.com>
2022-03-14 15:06:44 -0700

    Correction for nuber 1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,21 +9,23 @@
 int main(void)
 {
 	int n, m, l;
-	
-	for (m = 49; m < 58; m++)
-	{	
-		for (l = 50; l < 58; l++)
-		{
-			if (l > m && m > n)
+
+	for (n = 48; m < 48; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{	
+			for (l = 50; l < 58; l++)
 			{
-				putchar(n);
-				putchar(m);
-				putchar(l);
-				if (n != 55 || m != 56)
+				if (l > m && m > n)
 				{
-					putchar(',');
-					putchar(' ');
-					
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}	
 				}
 			}
 		}
