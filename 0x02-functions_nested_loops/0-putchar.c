1dbf85e3a01a317a7db9c95d10b2b413909bb9ef
gabbismart <brightgabi@gmail.com>
2022-03-16 14:06:12 -0700

    My putchar file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "Gabriel.h"
+
+/**
+ * main - prints Gabriel, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	-putchar('G');
+	-putchar('a');
+	-putchar('b');
+	-putchar('r');
+	-putchar('i');
+	-putchar('e');
+	-putchar('l');
+	-putchare('\n');
+
+	return (0);
+}
